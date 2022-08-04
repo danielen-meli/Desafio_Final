d1926dddd7a2532ce3ad475c813279280c04bfac
Danielen <danielen.cestari@mercadolivre.com>
2022-08-04 10:39:08 -0300

    add estrutura básica do projeto

---
+++ src/main/java/com/meli/desafio_final/DesafioFinalApplication.java
@@ -0,0 +1,13 @@
+package com.meli.desafio_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meli/desafio_final/DesafioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.desafio_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
