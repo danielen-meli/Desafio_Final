7937d4699f6afcf50968ecca3093ed06dce3f4b7
maria-mamede <maria.mamede@mercadolivre.com>
2022-08-05 17:08:05 -0300

    commit inicial paula req02

---
+++ src/main/java/com/meli/desafio_final/controller/SellerAdController.java
@@ -0,0 +1,32 @@
+package com.meli.desafio_final.controller;
+
+import com.meli.desafio_final.model.SellerAd;
+import com.meli.desafio_final.model.enums.Category;
+import com.meli.desafio_final.service.SellerAdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SellerAdController {
+
+    @Autowired
+    private SellerAdService sellerAdService;
+
+    @GetMapping("/fresh-products")
+    public ResponseEntity<List<SellerAd>> getListProducts(){
+        if(sellerAdService.getAllProducts().isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(sellerAdService.getAllProducts());
+    }
+
+    @GetMapping("/fresh-products/{category}")
+    public ResponseEntity<List<SellerAd>> getProductsCategory(@RequestParam Category category){
+        return null;
+    }
+}
