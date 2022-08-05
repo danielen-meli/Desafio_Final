ebfafcfcf15abe71d8082cc659ea5b87a62bd09e
Danielen <danielen.cestari@mercadolivre.com>
2022-08-05 14:03:21 -0300

    inicio trabalho dupla Dani e Adao

---
+++ src/main/java/com/meli/desafio_final/controller/ShopOrderController.java
@@ -0,0 +1,16 @@
+package com.meli.desafio_final.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/fresh-products/orders")
+public class ShopOrderController {
+
+    @PostMapping()
+    public ResponseEntity<> createShopOrder
+
+}
