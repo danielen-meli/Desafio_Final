93edab229c3eeda15828cea274ce89afa34ae963
Danielen <danielen.cestari@mercadolivre.com>
2022-08-08 13:01:54 -0300

    add dados para testar

--- src/main/java/com/meli/desafio_final/model/enums/Category.java
+++ src/main/java/com/meli/desafio_final/model/enums/Category.java
@@ -1,5 +1,20 @@
 package com.meli.desafio_final.model.enums;
 
+import lombok.Getter;
+
 public enum Category {
-    FROZEN, REFRIGERATED, FRESH
+    FROZEN("FROZEN", -18, -12),
+    REFRIGERATED("REFRIGERATED", 3, 10),
+    FRESH("FRESH", 10, 15);
+
+    @Getter
+    private String name;
+    public double minimumTemperature;
+    public double maximumTemperature;
+
+    Category(String name, double minimumTemperature, double maximumTemperature) {
+        this.name = name;
+        this.minimumTemperature = minimumTemperature;
+        this.maximumTemperature = maximumTemperature;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,16 +12,20 @@ replace into section (section_id, category, section_capacity, section_temperatur
 replace into section (section_id, category, section_capacity, section_temperature, warehouse_warehouse_id) VALUES (2, 2, "300", "5", 1);
 replace into section (section_id, category, section_capacity, section_temperature, warehouse_warehouse_id) VALUES (3, 3, "350", "15", 1);
 
-replace into inbound_order( inbound_order_id, order_date, section_id) values (1, now(), 1);
+replace into inbound_order( inbound_order_id, order_date, section_id) values (1, now(), 3);
 
-replace into PRODUCT (product_id, category, product_name) VALUES (1, 1, "maca");
-replace into PRODUCT (product_id, category, product_name) VALUES (2, 1, "Pera");
+replace into PRODUCT (product_id, category, product_name) VALUES (1, 3, "maca");
+replace into PRODUCT (product_id, category, product_name) VALUES (2, 3, "Pera");
+replace into PRODUCT (product_id, category, product_name) VALUES (3, 1, "Peito de Frango");
 
 replace into USER (user_id, cpf, email, password, user_name) VALUES (1, "1234", "julia.heberle", "123", "julia");
+replace into USER (user_id, cpf, email, password, user_name) VALUES (2, "7654", "danielen@gmail.com", "987654", "danielen");
 
 replace into BUYER (buyer_id, user_user_id) VALUES (1, 1);
+replace into BUYER (buyer_id, user_user_id) VALUES (2, 2);
 
 replace into SHOP_ORDER (order_id, status, buyer_buyer_id) VALUES (1, 1, 1);
+replace into SHOP_ORDER (order_id, status, buyer_buyer_id) VALUES (2, 1, 1);
 
 replace into SELLER_AD (seller_ad_id, price, seller_id, product_product_id) VALUES (1, 4.0, null, 1);
 
