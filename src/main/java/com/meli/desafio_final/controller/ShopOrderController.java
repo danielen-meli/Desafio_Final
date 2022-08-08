3680b6b3da0e430501a4e93be0413b39694aac05
JuliaHeberle <julia.heberle@mercadolivre.com>
2022-08-08 13:46:06 -0300

    criando put shopOrder

--- src/main/java/com/meli/desafio_final/controller/ShopOrderController.java
+++ src/main/java/com/meli/desafio_final/controller/ShopOrderController.java
@@ -2,9 +2,13 @@ package com.meli.desafio_final.controller;
 
 import com.meli.desafio_final.controller.dto.ShopOrderDto;
 import com.meli.desafio_final.model.ShopOrder;
+import com.meli.desafio_final.model.enums.Status;
 import com.meli.desafio_final.service.ShopOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/orders")
@@ -17,5 +21,11 @@ public class ShopOrderController {
     private ShopOrderDto getById(@PathVariable long id){
         ShopOrder shop = service.getById(id);
         return ShopOrderDto.converter(shop);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ShopOrder> updateShopOrder(@PathVariable long id, @RequestBody Map<String, Status> changes){
+        return ResponseEntity.ok(service.updatePartial(id, changes));
+
     }
 }
--- src/main/java/com/meli/desafio_final/service/ShopOrderService.java
+++ src/main/java/com/meli/desafio_final/service/ShopOrderService.java
@@ -2,11 +2,13 @@ package com.meli.desafio_final.service;
 
 import com.meli.desafio_final.model.ShopOrder;
 import com.meli.desafio_final.model.ShopOrderItem;
+import com.meli.desafio_final.model.enums.Status;
 import com.meli.desafio_final.repository.ShopOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ShopOrderService {
@@ -16,5 +18,17 @@ public class ShopOrderService {
 
     public ShopOrder getById(long id){
         return repository.findById(id).get();
+    }
+
+    public ShopOrder updatePartial(long id, Map<String, Status> changes) {
+        ShopOrder shopOrder = getById(id);
+
+        changes.forEach( (attribute, value)-> {
+            switch (attribute){
+                case "status": shopOrder.setStatus(value); break;
+            }
+        });
+
+        return repository.save(shopOrder);
     }
 }
