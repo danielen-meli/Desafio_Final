b81f73e00db02d995a47cfeae36cea024d457f8f
Danielen <danielen.cestari@mercadolivre.com>
2022-08-09 17:28:39 -0300

    carrinho sendo gerado com sucesso

--- src/main/java/com/meli/desafio_final/repository/IBatchStockRepository.java
+++ src/main/java/com/meli/desafio_final/repository/IBatchStockRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IBatchStockRepository extends JpaRepository<BatchStock, Long> {
+    List<BatchStock> findAllBySellerAdSellerAdId (long SellerAdId);
+
 }
--- src/main/java/com/meli/desafio_final/service/ShopOrderService.java
+++ src/main/java/com/meli/desafio_final/service/ShopOrderService.java
@@ -1,30 +1,25 @@
 package com.meli.desafio_final.service;
 
+import com.meli.desafio_final.dto.OrderAdRequestDto;
+import com.meli.desafio_final.dto.ShopOrderRequestDto;
+import com.meli.desafio_final.dto.ShopOrderResponseDto;
 import com.meli.desafio_final.exception.BadRequestException;
-import com.meli.desafio_final.exception.ExceptionsDetails;
-import com.meli.desafio_final.exception.NotFoundException;
-import com.meli.desafio_final.exception.QuantityException;
-import com.meli.desafio_final.model.BatchStock;
-import com.meli.desafio_final.model.Buyer;
-import com.meli.desafio_final.model.ShopOrder;
+import com.meli.desafio_final.model.*;
 import com.meli.desafio_final.model.enums.Status;
+import com.meli.desafio_final.repository.ISellerAdRepository;
 import com.meli.desafio_final.repository.IShopOrderRepository;
 import com.meli.desafio_final.repository.IBuyerRepository;
 import com.meli.desafio_final.repository.IBatchStockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
+
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 
 
@@ -42,29 +37,71 @@ public class ShopOrderService {
     @Autowired
     private IBatchStockRepository batchStockRepository;
 
+    @Autowired
+    private ISellerAdRepository sellerAdRepository;
+
+    // verifica o buyer
+    private Buyer verifyBuyerExists(long id){
+        return buyerRepository.findById(id).orElseThrow(() -> new BadRequestException("Buyer does't exists"));
+    }
+
+    private void productStocksAvailable(List<OrderAdRequestDto> products){
+        products.forEach(orderRequest -> {
+            long sellerAdId = orderRequest.getSellerAdId();
+            int orderQuantity = orderRequest.getQuantity();
+            List<BatchStock> batchStocksFound = batchStockRepository.findAllBySellerAdSellerAdId(sellerAdId);
+
+            List<BatchStock> batchStocksWithDueDateAvailable = filterBatchStocksByDueDate(batchStocksFound);
+            isStockQuantityAvailable(batchStocksWithDueDateAvailable, orderQuantity);
+
+        });
+    }
+
+    // valida que a quantidade em stock é suficiente para a request
+    private void isStockQuantityAvailable(List<BatchStock> batchStockList, int orderQuantity){
+        double stockQuantityAvailable = batchStockList.stream().mapToDouble(BatchStock::getCurrentQuantity).sum();
+        if(orderQuantity > stockQuantityAvailable) {
+            throw new BadRequestException("Order quantity requested is bigger than available stock");
+        }
+    }
+
+    // Retorna apenas os BatchStocks que o due date é maior q 21
+    private List<BatchStock> filterBatchStocksByDueDate(List<BatchStock> batchStockList){
+         List<BatchStock> batchStockListValid = batchStockList.stream().filter(bs-> {
+            Period expirationDate = Period.between(bs.getDueDate(), LocalDate.now());
+            if (expirationDate.getDays() > 21){
+                return false;
+            }
+            return true;
+         }).collect(Collectors.toList());
+
+         if(batchStockListValid.isEmpty()){
+             throw new BadRequestException("There are no products available now. Sorry");
+         }
+         return batchStockListValid;
+    }
+
+    private ShopOrder save(ShopOrderRequestDto shopOrderRequestDto, Buyer buyer) {
+        List<ShopOrderItem> shopOrderItems = shopOrderRequestDto.getProducts().stream().map(order -> {
+            SellerAd sellerAd = sellerAdRepository.findById(order.getSellerAdId()).orElseThrow(() -> new BadRequestException("SellerAd Invalid"));
+            int quantity = order.getQuantity();
+            LocalDate date = shopOrderRequestDto.getDate();
+            return new ShopOrderItem(date, quantity, sellerAd);
+        }).collect(Collectors.toList());
+
+        ShopOrder shopOrder = new ShopOrder(shopOrderRequestDto, buyer, shopOrderItems);
+        return shopOrderRepository.save(shopOrder);
+
+    }
+
 
     @Transactional // save e importante ter rollback en caso de erro
-    public ShopOrder save(ShopOrder shopOrder) {
-        // verificar se o comprador está cadastrado:
-        Optional<Buyer> buyerExist = buyerRepository.findById(shopOrder.getBuyer().getBuyerId());
-        if (buyerExist.isEmpty()) throw new NotFoundException("Comprador não cadastrado!");
+    public ShopOrder insertNewShopOrder(ShopOrderRequestDto shopOrderRequestDto) {
+        Buyer buyer = verifyBuyerExists(shopOrderRequestDto.getBuyerId());
+        productStocksAvailable(shopOrderRequestDto.getProducts());
 
-        shopOrder.getShopOrderItem().forEach(product -> {
-            BatchStock productInfo = (BatchStock) batchStockRepository.findById(product.getId()).orElse(null);
-            // verificar a quantidade de cada produto da lista
-            assert productInfo != null;
-            if (productInfo.getCurrentQuantity() < product.getQuantity()) {
-                throw new QuantityException("Não há quantidade suficiente no estoque!");
-            }
-            // validacao da data de validade - poderia ser o period
-            long expirationDate = ChronoUnit.DAYS.between(productInfo.getDueDate(), now());
-            if (expirationDate < 21) {
-                throw new QuantityException("Validade é menor que 3 semanas!");
-            }
-        });
-
-        return shopOrderRepository.save(shopOrder);
-    } // depois q valida, o carrinho pode ser fechado - fazer isso num put pra mudar o status e ai atualiza o estoque
+        return save(shopOrderRequestDto, buyer);
+    }
 
 
 /*    @Transactional
