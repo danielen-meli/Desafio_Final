a24f53ceec421bb575fb1e307f454e1d4368bb0f
maria-mamede <maria.mamede@mercadolivre.com>
2022-08-10 16:49:38 -0300

    alterei funcoes no service de SellerAd e BatchStock

--- src/main/java/com/meli/desafio_final/service/BatchStockService.java
+++ src/main/java/com/meli/desafio_final/service/BatchStockService.java
@@ -2,7 +2,6 @@ package com.meli.desafio_final.service;
 
 import com.meli.desafio_final.dto.BatchStockDto;
 import com.meli.desafio_final.exception.NotFoundException;
-import com.meli.desafio_final.model.BatchStock;
 import com.meli.desafio_final.repository.IBatchStockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,17 +17,14 @@ public class BatchStockService implements IBatchStockService{
 
     @Override
     public List<BatchStockDto> getProductsInStock(long productId) {
-        List<BatchStockDto> listBatchDto = batchStockRepository.findAll().stream().
-                map(BatchStockDto::new).collect(Collectors.toList());
+        List<BatchStockDto> listDtoByCategory = batchStockRepository.findAll().stream().
+                map(BatchStockDto::new).filter(p -> p.getProductId() == productId).collect(Collectors.toList());
 
-        List<BatchStockDto> DtoByCategory = listBatchDto.stream().
-                filter(p -> p.getProductId() == productId).collect(Collectors.toList());
-
-        if(DtoByCategory.isEmpty()){
+        if(listDtoByCategory.isEmpty()){
             throw new NotFoundException("Não existem produtos em estoque.");
         }
 
-        return DtoByCategory;
+        return listDtoByCategory;
     }
 
     @Override
