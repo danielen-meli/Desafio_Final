0e2dd723e0c9c2bad8f6adeffe306efd701c77e0
Danielen <danielen.cestari@mercadolivre.com>
2022-08-11 14:09:34 -0300

    remove code not used

--- src/main/java/com/meli/desafio_final/model/enums/Category.java
+++ src/main/java/com/meli/desafio_final/model/enums/Category.java
@@ -3,20 +3,6 @@ package com.meli.desafio_final.model.enums;
 import lombok.Getter;
 
 public enum Category {
-/*    FROZEN("FROZEN", -18, -12), // 0
-    REFRIGERATED("REFRIGERATED", 3, 10), //1
-    FRESH("FRESH", 10, 15); //2
-
-    @Getter
-    private String name;
-    public double minimumTemperature;
-    public double maximumTemperature;
-
-    Category(String name, double minimumTemperature, double maximumTemperature) {
-        this.name = name;
-        this.minimumTemperature = minimumTemperature;
-        this.maximumTemperature = maximumTemperature;
-    } */
     FROZEN,
     REFRIGERATED,
     FRESH;
