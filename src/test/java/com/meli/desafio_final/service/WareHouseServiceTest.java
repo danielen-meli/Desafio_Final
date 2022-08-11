41e78a3c4c67dcdaeaa7273ee5025d95c3c16692
JuliaHeberle <julia.heberle@mercadolivre.com>
2022-08-11 14:03:00 -0300

    req 4 finalizado e com testes

--- src/test/java/com/meli/desafio_final/service/WareHouseServiceTest.java
+++ src/test/java/com/meli/desafio_final/service/WareHouseServiceTest.java
@@ -1,5 +1,6 @@
 package com.meli.desafio_final.service;
 
+import com.meli.desafio_final.exception.NotFoundException;
 import com.meli.desafio_final.model.Section;
 import com.meli.desafio_final.model.SellerAd;
 import com.meli.desafio_final.model.Warehouse;
@@ -27,6 +28,34 @@ class WareHouseServiceTest {
 
     @Test
     public void getQuantityProducts_returnTotalQuantity_WhenProductHaveQuantity(){
+        Section section = TestUtilGeneratorWareHouse.newSectionToWareHouse();
+        SellerAd sellerAd = SellerAd.builder()
+                .sellerAdId(1)
+                .build();
 
+
+        BDDMockito.when(warehouseRepository.getAllProducts(anyLong(), anyLong())).thenReturn(100l);
+
+        long warehouseFind = warehouseService.getTotalQuantitySellerAdinWareHouse(sellerAd, section.getWarehouse());
+
+        assertThat(warehouseFind).isEqualTo(100l);
+
+    }
+
+    @Test
+    public void verifyProduct_throwException_WhenProductNotHaveQuantity(){
+        Section section = TestUtilGeneratorWareHouse.newSectionToWareHouse();
+        SellerAd sellerAd = SellerAd.builder()
+                .sellerAdId(1)
+                .build();
+
+        BDDMockito.when(warehouseRepository.getAllProducts(anyLong(), anyLong())).thenReturn(0l);
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            warehouseService.getTotalQuantitySellerAdinWareHouse(sellerAd, section.getWarehouse());
+        });
+
+        assertThat(exception.getMessage().contains("Produto não existe em nenhum depósito"));
+        assertThat(exception.getClass()).isEqualTo(NotFoundException.class);
     }
 }
