f86c6df1c16b79bf76917ba84328c37a028bead4
Danielen <danielen.cestari@mercadolivre.com>
2022-08-16 16:45:32 -0300

    funcionando até aqui

--- src/main/java/com/meli/desafio_final/controller/AutenticacaoController.java
+++ src/main/java/com/meli/desafio_final/controller/AutenticacaoController.java
@@ -3,11 +3,13 @@ package com.meli.desafio_final.controller;
 import com.meli.desafio_final.config.TokenService;
 import com.meli.desafio_final.dto.TokenDTO;
 import com.meli.desafio_final.model.LoginRequest;
+import com.meli.desafio_final.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,14 @@ public class AutenticacaoController {
 		Authentication authentication = manager.authenticate(dadosLogin);
 		String token = tokenService.geraToken(authentication);
 		return ResponseEntity.ok(new TokenDTO(token, "Bearer"));
+	}
+
+	@PostMapping("login")
+	public ResponseEntity<String> cadastraUsuario(@RequestBody Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		usuario.setSenha(encoder.encode(usuario.getSenha()));
+
+		return null;
+
 	}
 }
